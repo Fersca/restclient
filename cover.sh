850cbd801ce212a48d21535f2f814e064648900d
Fernando Scasserra <fersca@hotmail.com>
2016-08-22 17:11:20 -0300

    Update cover.sh

--- cover.sh
+++ cover.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-go test -coverprofile=coverage.out -coverpkg github.com/mercadolibre/go-meli-toolkit/restclient
+go test -coverprofile=coverage.out -coverpkg github.com/Fersca/restclient
 go tool cover -html=coverage.out
